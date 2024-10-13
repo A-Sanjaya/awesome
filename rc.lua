2ac6367c6d07a83fb98cf860a01f26dcdc81b8bd
A-Sanjaya <arthasanjayagt@gmail.com>
2024-10-14 02:22:20 +0800

    add rounded corner

--- rc.lua
+++ rc.lua
@@ -504,6 +504,10 @@ client.connect_signal("manage", function (c)
     -- i.e. put it at the end of others instead of setting it master.
     -- if not awesome.startup then awful.client.setslave(c) end
 
+    c.shape = function(cr, width, height)
+        gears.shape.rounded_rect(cr, width, height, 30) -- Change 10 to adjust corner radius
+    end
+    
     if awesome.startup
       and not c.size_hints.user_position
       and not c.size_hints.program_position then
